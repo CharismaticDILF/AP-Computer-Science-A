8a9e328515b5dcec86112612b64f000c2f1bcd08
Rose McB <99897360+CharismaticDILF@users.noreply.github.com>
2024-09-24 11:06:27 -0700

    add files from in class

---
+++ AP-Computer-Science-Unit-1/OldLady.java
@@ -0,0 +1,24 @@
+//Rose McBurnett
+//Period 3
+//AP CS A
+//Old Lady
+public class OldLady
+{
+    public static void main(String[] args) {
+        //Prints the first stanza and calls last few lines
+        System.out.println("There was an old lady who swallowed a fly");
+        shellDie();
+        
+        //Prints second stanza
+    }
+    
+    public static void shellDie() {
+        //The repeated lines at the end of most of the stanzas will be refered to as stanza end
+        System.out.println("I don’t know why she");
+        System.out.println("swallowed the fly");
+        System.out.println("Perhaps she'll die");
+        System.out.println();
+    }
+    
+    
+}
