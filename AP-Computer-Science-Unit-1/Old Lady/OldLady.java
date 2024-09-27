0330dc582ed963cdfbb05294ab44c8a38389da9e
Rose McB <99897360+CharismaticDILF@users.noreply.github.com>
2024-09-27 11:36:28 -0700

    Update OldLady.java

--- AP-Computer-Science-Unit-1/Old Lady/OldLady.java
+++ AP-Computer-Science-Unit-1/Old Lady/OldLady.java
@@ -5,20 +5,32 @@
 public class OldLady
 {
     public static void main(String[] args) {
+        firstStanza();
+    }
+
+    //Repeated lines for the food chain
+    public static void spiderEatFly(){
+        System.out.println("She swallowed the spider to catch the fly");
+    }
+    public static void birdEatSpider(){
+        System.out.println("She swallowed the bird to catch the spider");
+    }
+
+    public static void secondStanza(){  
+        //Prints the first stanza and calls last few lines
+        System.out.println("There was an old lady who swallowed a spider");
+        System.out.println("That wiggled and jiggled and tickled inside her");
+        shellDie();
+    }
+    public static void shellDie() {
+        //The repeated lines at the end of most of the stanzas will be refered to as stanza end
+        System.out.println("I don't know why she");
+        System.out.println("swallowed the fly");
+        System.out.println("Perhaps she'll die\n");
+    }
+    public static void firstStanza(){  
         //Prints the first stanza and calls last few lines
         System.out.println("There was an old lady who swallowed a fly");
         shellDie();
-        
-        //Prints second stanza
-    }
-    
-    public static void shellDie() {
-        //The repeated lines at the end of most of the stanzas will be refered to as stanza end
-        System.out.println("I don’t know why she");
-        System.out.println("swallowed the fly");
-        System.out.println("Perhaps she'll die");
-        System.out.println();
-    }
-    
-    
+    } 
 }
